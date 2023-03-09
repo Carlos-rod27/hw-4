05be83216866809ae881e0cd5ad460b310f5ef9c
Carlos_rod27 <rodriguec38@jacks.sfasu.edu>
2023-03-09 16:43:11 -0600

    doneto string added jsut for testing

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -9,19 +9,37 @@ public class PriorityQueue<T> implements IPriorityQueue<T> {
     @Override
     public void add(int priority, T item) {
         Node<T> newNode = new Node<T>(priority, item);
-        if(isEmpty()){
+        if (isEmpty()) {
             this.front = newNode;
+        } else {
+            Node<T> currentNode = this.front;
+            Node<T> lastNode = null;
+            while (currentNode != null) {
+                if (currentNode.getPriority() < priority) {
+                    break;
+                }
+                lastNode = currentNode;
+                currentNode = currentNode.getNext();
+            }
+            if (lastNode == null) {
+                newNode.setNext(front);
+                this.front = newNode;
+            } else {
+                lastNode.setNext(newNode);
+                newNode.setNext(currentNode);
+            }
         }
-        else{
-            this.front.setNext(newNode);
-        }
-        this.front = newNode;
     }
 
     @Override
     public T removeTopPriority() throws PQException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeTopPriority'");
+        if (this.isEmpty()) {
+            throw new PQException("This queue is empty!");
+        } else {
+            T item = front.getItem();
+            this.front = front.getNext();
+            return item;
+        }
     }
 
     @Override
@@ -31,6 +49,18 @@ public class PriorityQueue<T> implements IPriorityQueue<T> {
         } else {
             return this.front.getItem();
         }
+    }
+
+    @Override
+    public String toString() {String s = "[";
+    Node<T> current = this.front;
+    while (current != null) {
+        s = s + current.getItem().toString();
+        if (current.getNext() != null)
+            s += ", ";
+        current = current.getNext();
+    }
+    return s + "]";
     }
 
 }
--- Test.java
+++ Test.java
@@ -1,14 +1,26 @@
 public class Test {
     public static void main(String[] args) {
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+        PriorityQueue<String> queue = new PriorityQueue<String>();
         System.out.println("is the queue empty???" + queue.isEmpty());
         System.out.println("");
-        for (int i = 0; i < 5; i++)
-            queue.add(i, i);
+        queue.add(2, "Steven");
+        queue.add(1, "Alan");
+        queue.add(2, "pam");
+        queue.add(3, "cheese");
+        queue.add(4, "carlos");
+        queue.add(2, "lui");
+        queue.add(6, "gerardo");
+        queue.add(7, "dale");
         System.out.println("Adding Test");
         System.out.println(queue);
         System.out.println("");
         System.out.println("is the queue empty now????" + queue.isEmpty());
+        System.out.println("");
+        System.out.println("peek test");
+        System.out.println(queue.peekTopPriority());
+        System.out.println("");
+        System.out.println("remove test");
+        System.out.println(queue.removeTopPriority());
     }
 
 }
